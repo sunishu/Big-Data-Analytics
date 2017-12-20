d45ad3dabad776826043871cf12f200b4adcd401
sunishu <ss10448@nyu.edu>
2017-12-20 01:05:08 -0500

    Add files via upload

---
+++ Data Analysis/Location analysis of Crimes/premises.py
@@ -0,0 +1,90 @@
+from __future__ import print_function
+from pyspark import SparkContext
+import sys
+from operator import add
+from csv import reader
+
+def toCSVLine(data):
+    return ','.join(str(d) for d in data)
+
+
+if __name__ == '__main__':
+	
+	sc = SparkContext()
+	lines = sc.textFile(sys.argv[1], 1)
+
+	lines = lines.mapPartitions(lambda x: reader(x))
+	header = lines.first() #extract header
+    lines = lines.filter(lambda x : x!= header)
+
+    # for all boroughs
+    overall_a = lines.map(lambda x: (x[1], x[2], x[5], x[7], x[9], x[10], x[11], x[12], x[13], x[16]))
+    overall = overall_a.map(lambda x: (x[3], x[16]))
+    count_overall = overall.map(lambda x: (x,1)).reduceByKey(add).sortByKey().collect()
+    count_overall = sc.parallelize(count_overall)
+    count_overall = count_overall.map(lambda x: (str(x).replace('(',' ').replace('"',' ').replace(')',' ')))
+    final_overall = count_overall.map(toCSVLine)
+    final_overall = final_overall.map(lambda x: (str(x).replace('(','').replace(')','').replace(',','').replace("' ",',').replace("'", '')))
+    final_overall.saveAsTextFile('overall_Premises.csv')
+
+    # for Bronx
+    bronx_a = lines.filter(lambda x: x[13] == 'BRONX')
+    bronx_b = bronx_a.map(lambda x: (x[1], x[2], x[5], x[7], x[9], x[10], x[11], x[12], x[13]))
+    bronx = bronx_b.map(lambda x: (x[3], x[16]))
+    count_bronx = bronx.map(lambda x: (x,1)).reduceByKey(add).sortByKey().collect()
+    count_bronx = sc.parallelize(count_bronx)
+    count_bronx = count_bronx.map(lambda x: (str(x).replace('(',' ').replace('"',' ').replace(')',' ')))
+    final_bronx = count_bronx.map(toCSVLine)
+    final_bronx = final_bronx.map(lambda x: (str(x).replace('(','').replace(')','').replace(',','').replace("' ",',').replace("'", '')))
+    final_bronx.saveAsTextFile('bronx_Premises.csv')
+
+
+    # for brooklyn
+    brooklyn_a = lines.filter(lambda x: x[13] == 'BROOKLYN')
+    brooklyn_b = brooklyn_a.map(lambda x: (x[1], x[2], x[5], x[7], x[9], x[10], x[11], x[12], x[13]))
+    brooklyn = brooklyn_b.map(lambda x: (x[3], x[16]))
+    count_brooklyn = brooklyn.map(lambda x: (x,1)).reduceByKey(add).sortByKey().collect()
+    count_brooklyn = sc.parallelize(count_brooklyn)
+    count_brooklyn = count_brooklyn.map(lambda x: (str(x).replace('(',' ').replace('"',' ').replace(')',' ')))
+    final_brooklyn = count_brooklyn.map(toCSVLine)
+    final_brooklyn = final_brooklyn.map(lambda x: (str(x).replace('(','').replace(')','').replace(',','').replace("' ",',').replace("'", '')))
+    final_brooklyn.saveAsTextFile('brooklyn_Premises.csv')
+
+
+    # for manhattan
+    manhattan_a = lines.filter(lambda x: x[13] == 'MANHATTAN')
+    manhattan_b = manhattan_a.map(lambda x: (x[1], x[2], x[5], x[7], x[9], x[10], x[11], x[12], x[13]))
+    manhattan = manhattan_b.map(lambda x: (x[3], x[16]))
+    count_manhattan = manhattan.map(lambda x: (x,1)).reduceByKey(add).sortByKey().collect()
+    count_manhattan = sc.parallelize(count_manhattan)
+    count_manhattan = count_manhattan.map(lambda x: (str(x).replace('(',' ').replace('"',' ').replace(')',' ')))
+    final_manhattan = count_manhattan.map(toCSVLine)
+    final_manhattan = final_manhattan.map(lambda x: (str(x).replace('(','').replace(')','').replace(',','').replace("' ",',').replace("'", '')))
+    final_manhattan.saveAsTextFile('manhattan_Premises.csv')
+
+
+    # for queens
+    queens_a = lines.filter(lambda x: x[13] == 'QUEENS')
+    queens_b = queens_a.map(lambda x: (x[1], x[2], x[5], x[7], x[9], x[10], x[11], x[12], x[13]))
+    queens = queens_b.map(lambda x: (x[3], x[16])) 
+    count_queens = queens.map(lambda x: (x,1)).reduceByKey(add).sortByKey().collect()
+    count_queens = sc.parallelize(count_queens)
+    count_queens = count_queens.map(lambda x: (str(x).replace('(',' ').replace('"',' ').replace(')',' ')))
+    final_queens = count_queens.map(toCSVLine)
+    final_queens = final_queens.map(lambda x: (str(x).replace('(','').replace(')','').replace(',','').replace("' ",',').replace("'", '')))
+    final_queens.saveAsTextFile('queens_Premises.csv')
+
+
+    # for staten island
+    staten_a = lines.filter(lambda x: x[13] == 'STATEN ISLAND')
+    staten_b = staten_a.map(lambda x: (x[1], x[2], x[5], x[7], x[9], x[10], x[11], x[12], x[13]))
+    staten = staten_b.map(lambda x: (x[3], x[16])) 
+    count_staten = staten.map(lambda x: (x,1)).reduceByKey(add).sortByKey().collect()
+    count_staten = sc.parallelize(count_staten)
+    count_staten = count_staten.map(lambda x: (str(x).replace('(',' ').replace('"',' ').replace(')',' ')))
+    final_staten = count_staten.map(toCSVLine)
+    final_staten = final_staten.map(lambda x: (str(x).replace('(','').replace(')','').replace(',','').replace("' ",',').replace("'", '')))
+    final_staten.saveAsTextFile('staten_Premises.csv')
+
+
+    sc.stop()
